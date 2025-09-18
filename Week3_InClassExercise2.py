31d5d73babd4b3e5871edcac5936687748f8c3b8
athicomel <anhaszeli@gmail.com>
2025-09-18 17:33:15 +0900

    Add files via upload

---
+++ Week3_InClassExercise2.py
@@ -0,0 +1,25 @@
+class A:
+    def hello(self):
+        print("Hello from A")
+class B(A):
+    def hello(self):
+        print("Hello from B")
+        super().hello()
+class C(A):
+    def hello(self):
+        print("Hello from C")
+        super().hello()
+class D(B, C):
+    def hello(self):
+        print("Hello from D")
+        super().hello()
+
+class E(C):
+    pass
+
+class F(B, E):
+    pass
+
+class G(C, D):
+    pass
+print(G.mro())
